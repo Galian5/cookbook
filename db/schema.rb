c99fddf94e2f98d056bd0b8c602a54bee8c215f8
Galian <galian5@wp.pl>
2018-05-10 17:43:21 +0200

    Add basic users controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180508180043) do
+ActiveRecord::Schema.define(version: 20180509211833) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20180508180043) do
     t.text "equipment"
     t.text "ingredients"
     t.text "directions"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

15cd257d98d4b032939bdfd5d8f24972d0118374
Galian <galian5@wp.pl>
2018-05-22 15:42:02 +0200

    Add user validation with tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
+require 'test_helper'
+
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 100 },
+            format: { with: VALID_EMAIL_REGEX},
+            uniqueness: {case_sensitive: false}
   has_many :recipes
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,50 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: 'Example', email: 'user@example.com')
+  end
+
+  test 'should be valid' do
+    assert @user.valid?
+  end
+
+  test 'name should not be blank' do
+    @user.name = '      '
+    assert_not @user.valid?
+  end
+
+  test 'name should not be too long' do
+    @user.name = 'x' * 51
+    assert_not @user.valid?
+  end
+
+  test 'email should not be too long' do
+    @user.email = 'x' * 100 + '@example.com'
+    assert_not @user.valid?
+  end
+
+  test 'email should not be blank' do
+    @user.email = '     '
+    assert_not @user.valid?
+  end
+
+  test 'email should be unique' do
+    non_unique = @user.dup
+    non_unique.email = @user.email.downcase
+    @user.save
+    assert_not non_unique.valid?
+  end
+
+  # this test is copied, but seems to work
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+
 end
